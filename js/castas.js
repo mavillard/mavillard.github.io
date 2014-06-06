4c4ea716b7260e783259d79edd87e748eff87965
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2014-06-06 04:03:07 -0400

    Adding data

--- js/castas.js
+++ js/castas.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
-//var table = $('<table></table>').addClass('castas-table');
-//for(i=0; i<3; i++){
-//    var row = $('<tr></tr>').addClass('bar').text('result ' + i);
-//    table.append(row);
-//}
+var table = $("<table></table>").addClass("castas-table");
+for(i=0; i<3; i++){
+    var row = $("<tr></tr>").addClass("bar").text("result " + i);
+    table.append(row);
+}
 
-//$('#here_table').append(table);
+$("#castas-content").append(table);
 
 });
