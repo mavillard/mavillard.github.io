53951710043e379bdfab211ab7aae4df61b02717
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2014-06-06 05:14:45 -0400

    Changing format data

--- js/castas.js
+++ js/castas.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
-
-var table = $("<table></table>").addClass("castas-table");
-for(i=0; i<3; i++){
-    var row = $("<tr></tr>").addClass("bar").text("result " + i);
-    table.append(row);
-}
-
-$("#castas-content").append(table);
-
+  var table = $("<table></table>").addClass("castas-table");
+  var i = 1;
+  cond = true
+  while(cond) {
+    $.ajax({
+      url: "/data/painting/" + i + ".html",
+      dataType: "html",
+      success: function(data, status) {
+        var row = $("<tr></tr>").addClass("castas-row").text("result " + i);
+        table.append(row);
+        i += 1;
+      },
+      error: function(data, status) {
+        cond = false;
+      }
+    });
+  }
+  $("#castas-content").append(table);
 });
