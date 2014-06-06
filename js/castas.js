6e18b7978f8870d0e5cb9525045a4f6e7ad80bc3
Antonio Jimenez Mavillard <ajimene6@uwo.ca>
2014-06-06 06:03:53 -0400

    json-ld embedded

--- js/castas.js
+++ js/castas.js
@@ -1,20 +1,36 @@
 $(document).ready(function(){
   var table = $("<table></table>").addClass("castas-table");
-  var i = 1;
-  cond = true
-  while(cond) {
-    $.ajax({
-      url: "/data/painting/" + i + ".html",
-      dataType: "html",
-      success: function(data, status) {
-        var row = $("<tr></tr>").addClass("castas-row").text("result " + i);
-        table.append(row);
-        i += 1;
-      },
-      error: function(data, status) {
-        cond = false;
-      }
-    });
+  for(var i = 1; i <= 48; i++) {
+    (function(k) {
+      $.ajax({
+        url: "file:///home/antonio/git/lod/data/painting/" + k + ".json",
+        dataType: "json",
+        success: function(data, status) {
+          var row = $("<tr></tr>").addClass("castas-row");
+          var col1 = $("<td></td>").addClass("castas-col").text(data['title']);
+          var col2 = $("<td></td>").addClass("castas-col").text(data['author']);
+          var col3 = $("<td></td>").addClass("castas-col").text(data['year']);
+          row.append(col1);
+          row.append(col2);
+          row.append(col3);
+          table.append(row);
+        },
+        error: function(data, status) {
+          console.log('Error');
+        }
+      });
+    })(i);
   }
   $("#castas-content").append(table);
+  
+  $.ajax({
+    url: "file:///home/antonio/git/lod/castas_lod.json",
+    dataType: "text",
+    success: function(data, status) {
+      $("#castas-jsonld").text(data);
+    },
+    error: function(data, status) {
+      console.log('Error');
+    }
+  });
 });
